8426b5d7715dcf4377fb0adb9881121356a2d602
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2025-06-05 08:41:57 +0530

    Fitness Class Listing API

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,15 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import FitnessClass, Booking
+from django.utils.timezone import now
+from .serializers import FitnessClassSerializer, BookingSerializer
+from rest_framework.response import Response
 
 # Create your views here.
+class FitnessClassListing(APIView):
+    def get(self, request):
+        classes = FitnessClass.objects.filter(datetime__gte=now()).order_by('datetime')
+        serializer = FitnessClassSerializer(classes, many=True)
+        return Response(serializer.data)
+
