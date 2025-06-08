4eaedd0deced551f99ada45d68e0aec6f2b24176
Akbar Ali <akbaralipanthakkanekd@gmail.com>
2025-06-08 11:14:28 +0530

    Unit test implementation

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('classes/', FitnessClassListing.as_view()),
-    path('book-class/', BookClass.as_view()),
+    path('book-class/', BookClass.as_view(), name='book-class'),
     path('bookings/', Bookings.as_view())
 ]
